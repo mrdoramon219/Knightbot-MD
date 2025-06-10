cd544695edec17c4d3d0523eaa92044c7a888cf0
mrdoramon219 <mrdoramon219@gmail.com>
2025-06-10 08:00:42 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Mr Doramon',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Mr Doramon",
+  botOwner: 'Mr Doramon', // Your name
+  ownerNumber: '923288666219', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
